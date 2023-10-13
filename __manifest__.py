c807679a09da979d68c6ca96b8f9bc62cb2b345b
AimFried <julien40.der@gmail.com>
2023-10-13 08:35:49 +0200

    init

---
+++ __manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cineplex",
+
+    'summary': """
+        Gestion de salles de cinema""",
+
+    'description': """
+        salles de cinema
+        seances de cinema
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Custom',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/templates.xml',
+        'views/city.xml',
+        'views/site.xml',
+        'views/salle.xml',
+        'views/seance.xml',
+        'views/film.xml',
+        'views/views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ models/models.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, exceptions
+
+class City(models.Model):
+    _name = 'cineplex.city'
+
+    name = fields.Char(required=True)
+    cp = fields.Integer(string="Code Postal",required=True)
+
+
+class Site(models.Model):
+    _name = 'cineplex.site'
+
+    name = fields.Char(required=True)
+    city_id = fields.Many2one('cineplex.city', ondelete='set null', string='City', index=True)
+
+class Salle(models.Model):
+    _name = 'cineplex.salle'
+    
+    name = fields.Char(required=True)
+    nb_place = fields.Integer(string = "Nombre de place")
+    site_id = fields.Many2one('cineplex.site', ondelete='set null', string='Site', index=True)
+    nb_place_rest=fields.Integer(string = "Nombre de place restees", compute="_nb_place_rest")
+    seance_id = fields.One2many('cineplex.seance', 'salle_id',string="Seance")
+    
+
+    @api.depends('nb_place', 'seance_id.seats')
+    def _nb_place_rest(self):
+        for r in self:
+            r.nb_place_rest = r.nb_place - r.seance_id.seats
+ 
+    @api.constrains('nb_place')
+    def _verify_nb_place(self):
+        for r in self:
+            if r.nb_place <= 0:
+                raise exceptions.ValidationError("Le Nombre de place doit être supérieur à 0")
+
+
+class Film(models.Model):
+    _name = 'cineplex.film'
+
+    name = fields.Char(string="Titre",required=True)    
+
+class Seance(models.Model):
+    _name = 'cineplex.seance'
+
+   
+    start_date = fields.Date(string="Date de debut",default=fields.Date.today)
+    duration = fields.Float(string="Duration in hours", help="Duree en heure (1,5 = 1h et 30 minutes)",required=True)
+    seats = fields.Integer(string = "Nombre de place reservees", required=True)
+
+    salle_id = fields.Many2one('cineplex.salle', ondelete='set null', string='Salle', index=True)
+    film_id = fields.Many2one('cineplex.film', ondelete='set null', string='Film', index=True)
+
+    @api.constrains('seats', 'duration')
+    def _verify_seats(self):
+        for r in self:
+            if r.salle_id.nb_place - r.seats < 0 or r.seats <= 0:
+                raise exceptions.ValidationError("=>Verifier la disponibilite des places!\n=>Le Nombre de place a reserve doit etre superieur a 0")
+            if r.duration == 0:
+                raise exceptions.ValidationError("La Duree doit etre sup a 0")
+    
+# class cineplex(models.Model):
+#     _name = 'cineplex.cineplex'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
